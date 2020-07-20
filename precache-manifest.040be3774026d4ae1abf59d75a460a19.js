ddf6aeabca30a613eb682e5535965b518267bc75
Siken <sikenn@pm.me>
2020-07-21 01:05:44 +0200

    Updates

---
+++ precache-manifest.040be3774026d4ae1abf59d75a460a19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "854def2f56b8c17a122c2e6fef019a6a",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "aa32721492e3b044b268",
+    "url": "/random-quote-machine/static/css/2.8ddda0ad.chunk.css"
+  },
+  {
+    "revision": "0208bc0e4ed0a200ec01",
+    "url": "/random-quote-machine/static/css/main.c05c4af3.chunk.css"
+  },
+  {
+    "revision": "aa32721492e3b044b268",
+    "url": "/random-quote-machine/static/js/2.efbb4e27.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-machine/static/js/2.efbb4e27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0208bc0e4ed0a200ec01",
+    "url": "/random-quote-machine/static/js/main.9eaf48a8.chunk.js"
+  },
+  {
+    "revision": "79be2feea42a5a087d5f",
+    "url": "/random-quote-machine/static/js/runtime-main.630878ba.js"
+  }
+]);
